935ed21183bdfc2c004a481e2411dd227b26d1da
fafusha <dr@MacBook-Ellina.local>
2020-01-14 11:48:15 -0500

    update

---
+++ TUT/tut2note.r
@@ -0,0 +1,22 @@
+# Numeric Vectors
+
+# rnorm(n)
+n <- 5
+rnorm(n)
+
+# :
+1:n
+
+# seq(a, b, by = c)
+a <- 3
+b <- 12
+c <- 2
+seq(a, b, by = c)
+
+# rep(a, n)
+rep(a, n)
+
+#Character Vectors
+
+# paste() concatenates/combines characrer strings
+
